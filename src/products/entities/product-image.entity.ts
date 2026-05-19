f2a342dc9ebbc9337a9eda88c9851d882f290a4d
SebaGarea <sebastiangarea@outlook.com>
2026-05-19 19:51:21 -0300

    feat: set explicit table names for Product and ProductImage entities

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity.js';
 
-@Entity()
+@Entity({ name: 'product_images' })
 export class ProductImage {
   @PrimaryGeneratedColumn()
   id: number;
